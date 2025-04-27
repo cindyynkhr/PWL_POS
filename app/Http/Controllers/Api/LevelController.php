e91d3052d106d6e98c969dbbd7d313d8e8223742
Cindy Nur Khoiriyah <cindic455@gmail.com>
2025-04-28 02:41:08 +0700

    Jb9_Praktikum 4

--- app/Http/Controllers/Api/LevelController.php
+++ app/Http/Controllers/Api/LevelController.php
@@ -4,8 +4,44 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\LevelModel;
 
 class LevelController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $query = LevelModel::query();
+
+        if ($request->has('ADM')) {
+            $query->where('ADM', $request->ADM);
+        }
+
+        return response()->json($query->get());
+    }
+
+    public function store(Request $request)
+    {
+        $level = LevelModel::create($request->all());
+        return response()->json($level, 201);
+    }
+
+    public function show(LevelModel $level)
+    {
+        return response()->json($level);
+    }
+
+    public function update(Request $request, LevelModel $level)
+    {
+        $level->update($request->all());
+        return response()->json($level);
+    }
+
+    public function destroy(LevelModel $level)
+    {
+        $level->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data terhapus',
+        ]);
+    }
 }
